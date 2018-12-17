8756c9393ed164069f11c3ffad7ac6a48d8b2eca
lilingjie <lilingjie1992@gmail.com>
2018-12-17 14:01:34 +0800

    add xcode project

---
+++ NewPrivatePod/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NewPrivatePod
+//
+//  Created by Kede on 2018/12/17.
+//  Copyright © 2018 clare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
